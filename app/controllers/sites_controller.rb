d3b4f307884780e6a5ba20531247951f6ecab9e6
Michael Wenger <michaelwenger27@gmail.com>
2014-09-20 16:05:16 -0400

    Removed header from sites pages

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,10 +1,13 @@
 class SitesController < ApplicationController
   def jed
+  	render layout: false
   end
 
   def upworthy
+  	render layout: false
   end
 
   def nami
+  	render layout: false
   end
 end
