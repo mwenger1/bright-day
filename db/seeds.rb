039a304c1719398cbdc18ad024fb7d003d295416
Michael Wenger <michaelwenger27@gmail.com>
2014-09-20 16:56:26 -0400

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Adding depressed individuals"
+Depressed.create(first_name: 'Mike', last_name: 'Wenger', age: 27, relation: "Friend", interests: "Snowboarding, U2, Giants")
+Depressed.create(first_name: 'Hannah', last_name: 'Oppeneheimer', age: 21, relation: "Daughter", interests: "Dispatch, Soccer")
+puts "Finished adding depressed individuals"
